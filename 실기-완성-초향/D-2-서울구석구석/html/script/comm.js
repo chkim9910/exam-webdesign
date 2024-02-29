2516e9df2d3c94001360b1b906293451afac297c
김초향 <chkim9910@naver.com>
2024-02-29 13:01:01 +0900

    update

--- 실기-완성-초향/D-2-서울구석구석/html/script/comm.js
+++ 실기-완성-초향/D-2-서울구석구석/html/script/comm.js
@@ -1,22 +1,49 @@
 $(function () {
-  setInterval(function () {
-    slide();
+  // setInterval(() => {
+  //   slide();
+  // }, 3000);
+  // // setInterval(function () {
+  // //   slide();
+  // // }, 3000);
+
+  // let now = 1;
+  // const slideWrap = $(".slide-wrap");
+  // const slideCount = $(".slide-wrap .slide-el").length;
+
+  // function slide() {
+  //   if (now < slideCount) {
+  //     // slideWrap.animate({
+  //     //   left: now * -100 + "%",
+  //     // });
+  //     slideWrap.css({ left: now * -100 + "%", transition: "all 0.3s" });
+  //     now += 1;
+  //   } else {
+  //     // slideWrap.animate({
+  //     //   left: 0,
+  //     // });
+  //     slideWrap.css("left", 0);
+  //     now = 1;
+  //   }
+  // }
+
+  // slide
+  let now = 0;
+  const slideWrap = $(".slide-wrap");
+  const slideCount = $(".slide-wrap .slide-el").length;
+
+  setInterval(() => {
+    now = (now + 1) % slideCount;
+    slideWrap.css({ left: now * -100 + "%", transition: "all 0.3s" });
   }, 3000);
 
-  let now = 1;
-  const slideWrap = $(".slide-wrap");
+  // tab
+  tabItem = $(".tab-item");
+  tabItem.first().parents(".tab").addClass("active");
+  tabItem.on("click", function (event) {
+    // 클릭 이벤트 방지
+    event.preventDefault();
 
-  function slide() {
-    if (now < 3) {
-      slideWrap.animate({
-        left: now * -100 + "%",
-      });
-      now += 1;
-    } else {
-      slideWrap.animate({
-        left: 0,
-      });
-      now = 1;
-    }
-  }
+    tabItem.parents(".tab").removeClass("active");
+    $(this).parents(".tab").addClass("active");
+  });
 });
