adbf31e1f5e782a39d5b38fa61dc5172d20410bb
김초향 <chkim9910@naver.com>
2024-03-19 18:13:43 +0900

    add slide animation fadein

--- 실기-완성-초향/C-1-해운대빛축제/js/comm.js
+++ 실기-완성-초향/C-1-해운대빛축제/js/comm.js
@@ -1,16 +1,20 @@
 $(function () {
-  slideLength = $(".slide-el").length;
-  slideBox = $(".slide-box");
-  slide = $(".slide-el");
-  console.log(slideLength);
+    let now = 0
+    const slideLength = $('.slide-el').length
+    const slideBox = $('.slide-box')
+    console.log(slideLength)
 
-  let now = 1;
+    // 슬라이드 페이드인아웃
+    setInterval(function () {
+        next = (now + 1) % slideLength
+        slideBox.find('li').eq(now).fadeOut(400)
+        slideBox.find('li').eq(next).fadeIn(400)
+        now = next
+    }, 3000)
 
-  if (now % slideLength < slideLength) {
-    console.log(now % slideLength);
-    slideBox.css({ left: -100 * now + "%" });
-    now += 1;
-  } else {
-    now = 0;
-  }
-});
+    // 슬라이드 상하
+    // setInterval(function () {
+    //     now = (now + 1) % slideLength
+    //     slideBox.css({ top: now * -100 + '%', transition: 'all, 0.3s' })
+    // }, 3000)
+})
